d64941e5eb76e4b9b9f3269f5c3482f6ee6b8cc8
Peter Kim <tweaked540@gmail.com>
2015-02-04 11:19:10 -0800

    Initial Update

---
+++ popup.ps1
@@ -0,0 +1,19 @@
+function Auth_Popup
+{
+<#
+.SYNOPSIS
+Modified Nishang script which opens a user credential prompt.
+.LINK
+Original Script and credit
+https://github.com/samratashok/nishang
+#>
+[CmdletBinding()]
+Param ()
+$credential = $host.ui.PromptForCredential("Credentials are required to perform this operation", "Please enter your username and password.", "", "")
+$creds = $credential.GetNetworkCredential()
+[String]$user = $creds.username
+[String]$pass = $creds.password
+write-Output $user
+write-Output $pass
+}
+Auth_Popup
